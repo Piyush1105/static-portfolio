ea78b2c9ced7044b89a649ece5b449e42cfc0455
Piyush Yadav <41717967+Piyush1105@users.noreply.github.com>
2022-01-24 17:07:29 +0530

    js change

--- index.js
+++ index.js
@@ -5,22 +5,11 @@ const display = document.querySelectorAll(".section-display");
 
 for (let i = 0; i < navigation.length; i++) {
   navigation[i].addEventListener("click", function () {
-    const innerText = navigation[i].textContent.toLowerCase();
-
-    const section = document
-      .querySelector("#" + innerText)
-      .classList.contains("active");
-
-    if (section === false) {
-      navigation[i].classList.add("active");
-      display[i].classList.add("active");
-      document.querySelector("#" + innerText).classList.add("active");
-      for (let j = 0; j < navigation.length; j++) {
-        if (j !== i) {
-          navigation[j].classList.remove("active");
-          display[j].classList.remove("active");
-        }
-      }
+    for (let j = 0; j < navigation.length; j++) {
+      navigation[j].classList.remove("active-link");
+      display[j].classList.remove("active-section");
     }
+    navigation[i].classList.add("active-link");
+    display[i].classList.add("active-section");
   });
 }
