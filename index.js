b3e013e8baeeececee5a121d89ce42054679882d
Piyush Yadav <41717967+Piyush1105@users.noreply.github.com>
2022-01-25 18:15:18 +0530

    new way

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
 "use Strict";
-
-const navigation = document.querySelectorAll(".nav-link");
-const display = document.querySelectorAll(".section-display");
-
-for (let i = 0; i < navigation.length; i++) {
-  navigation[i].addEventListener("click", function () {
-    for (let j = 0; j < navigation.length; j++) {
-      navigation[j].classList.remove("active-link");
-      display[j].classList.remove("active-section");
-    }
-    navigation[i].classList.add("active-link");
-    display[i].classList.add("active-section");
-  });
-}
